f367feff4ee5c689a641083351a058a416e86d64
David Morris <david.morris@nefino.de>
2022-03-09 15:39:17 +0000

    DEV-2463: test if Buffer is breaking the middleware

--- _middleware.ts
+++ _middleware.ts
@@ -1,16 +1,17 @@
-import {Buffer} from "buffer"
+// import {Buffer} from "buffer"
 
 export function middleware(req: Request) {
   const basicAuth = req.headers.get('authorization')
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
-    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+    return new Response()
+    // const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
-        // if we need more middleware, we have to figure out how to mimic NextResponse.next()
-      return new Response()
-    }
+    // if (user === '4dmin' && pwd === 'testpwd123') {
+    //     // if we need more middleware, we have to figure out how to mimic NextResponse.next()
+    //   return new Response()
+    // }
   }
 
   return new Response('Auth required', {
