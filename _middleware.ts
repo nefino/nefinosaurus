a71b26267b58feff50af5b5d4f92789a5732e3a3
David Morris <david.morris@nefino.de>
2022-03-18 14:17:10 +0000

    experimenting with simpler middleware

--- _middleware.ts
+++ _middleware.ts
@@ -1,18 +1,18 @@
 // import {Buffer} from "buffer"
 
 export function middleware(req: Request) {
-  const basicAuth = req.headers.get('authorization')
+  // const basicAuth = req.headers.get('authorization')
 
-  if (basicAuth) {
-    const auth = basicAuth.split(' ')[1]
-    return new Response()
-    // const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+  // if (basicAuth) {
+  //   const auth = basicAuth.split(' ')[1]
+  //   return new Response()
+  //   const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
-    // if (user === '4dmin' && pwd === 'testpwd123') {
-    //     // if we need more middleware, we have to figure out how to mimic NextResponse.next()
-    //   return new Response()
-    // }
-  }
+  //   if (user === '4dmin' && pwd === 'testpwd123') {
+  //       // if we need more middleware, we have to figure out how to mimic NextResponse.next()
+  //     return new Response()
+  //   }
+  // }
 
   return new Response('Auth required', {
     status: 401,
