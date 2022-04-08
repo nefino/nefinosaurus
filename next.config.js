372d47ab61213e13109ba7710a75445ccd7b8d06
Chris Stetter <chris.stetter@nefino.de>
2022-04-08 09:35:09 +0000

    DEV-2463 move rewrite to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: '/',
-          destination: '/index.html',
-        },
-      ]
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/django/:path*",
+        has: [
+          {
+            type: "host",
+            value: "docs.nefino.cc",
+          },
+        ],
+        destination: "https://django.docs.nefino.cc/:path*",
+      },
+      {
+        source: "/react/:path*",
+        has: [
+          {
+            type: "host",
+            value: "docs.nefino.cc",
+          },
+        ],
+        destination: "https://react.docs.nefino.cc/:path*",
+      },
+      {
+        source: "/airflow/:path*",
+        has: [
+          {
+            type: "host",
+            value: "docs.nefino.cc",
+          },
+        ],
+        destination: "https://airflow.docs.nefino.cc/:path*",
+      },
+      {
+        source: "/(.*)",
+        destination: "/index.html",
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
